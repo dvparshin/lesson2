8b16cc12b353aa1f4aa2fe5894f6c2eaad46c1f0
Dmitriy Parshin <parshyn.dima@gmail.com>
2024-09-13 14:52:31 +0300

    removed the nesting condition

--- 2_if2.py
+++ 2_if2.py
@@ -15,18 +15,17 @@
 
 """
 def check_string(first_string, second_string):
-    if isinstance(first_string, str) and isinstance(second_string, str):
-      if first_string == second_string:
-          return "1" # Строки одинаковые
+    if isinstance(first_string, str) and isinstance(second_string, str) and first_string == second_string:
+      print("1") # Строки одинаковые
     else:
-        return "0" # Строки одинаковые 
+        print("0") # Строки одинаковые 
     if first_string > second_string:
-        return "2" # Первая строка длиннее
-    if first_string < second_string:
-        return "3" # Встрока строка длиннее
+        print("2") # Первая строка длиннее
+    if first_string != second_string or second_string == 'learn':
+        return ("3") # Cтроки разные и вторая строка 'learn'
 
 def main():
-    result = check_string("11", "1")
+    result = check_string("1", "learn")
     print(result)
     
 if __name__ == "__main__":
