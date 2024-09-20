b1aceee4592cff4fda2469e617d2f05958886fa4
Dmitriy Parshin <parshyn.dima@gmail.com>
2024-09-20 13:32:24 +0300

    corrected the comments

--- 2_if2.py
+++ 2_if2.py
@@ -15,17 +15,18 @@
 
 """
 def check_string(first_string, second_string):
-    if isinstance(first_string, str) and isinstance(second_string, str) and first_string == second_string:
-      print("1") # Строки одинаковые
-    else:
-        print("0") # Строки одинаковые 
-    if first_string > second_string:
-        print("2") # Первая строка длиннее
-    if first_string != second_string or second_string == 'learn':
-        return ("3") # Cтроки разные и вторая строка 'learn'
+    if isinstance(first_string, str) == 0 or isinstance(second_string, str) == 0:
+      return 0
+    elif first_string == second_string:
+      return 1 # Строки одинаковые
+    elif first_string > second_string:
+        return 2 # Первая строка длиннее
+    elif first_string != second_string and second_string == 'learn':
+        return 3 # Cтроки разные и вторая строка 'learn'
+
 
 def main():
-    result = check_string("1", "learn")
+    result = check_string("1", 1)
     print(result)
     
 if __name__ == "__main__":
